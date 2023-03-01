5326734f07178e1e64dd531152dcf4152843a170
Ïliana <lizbeth.carvajalq@gmail.com>
2023-03-01 17:31:32 +0100

    Read csv from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas as pd
 streamlit.title('First Attempt Snowflake Api')
 
 streamlit.header('Breakfast Menu')
@@ -8,4 +9,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-    
+
+#reading csv from  s3 bucket
+myfruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruit_list)
