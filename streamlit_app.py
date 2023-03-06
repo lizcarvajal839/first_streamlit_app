51df84e4ca1981eab6e9df61b6a9b837c15664d1
Ïliana <lizbeth.carvajalq@gmail.com>
2023-03-06 14:52:30 +0100

    Error Fixing Cursor

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains")
 #function to get the fruit from snowflake 
 def get_fruit_load_list():
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
 
@@ -79,7 +79,7 @@ if streamlit.button('Get Fruit Load List'):
 
 
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return ('Thanks for adding',new_fruit)
 
