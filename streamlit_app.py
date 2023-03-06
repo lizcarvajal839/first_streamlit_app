99944dc26974758b4a4c43b2a2d128ec5eaf3357
Ïliana <lizbeth.carvajalq@gmail.com>
2023-03-06 12:31:23 +0100

    Try Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,18 +34,23 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        # transform json  to normalize
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        # show json data into table 
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
 
-# transform json  to normalize
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# show json data into table 
-streamlit.dataframe(fruityvice_normalized)
+
 
 #dont run anything 
 streamlit.stop()
